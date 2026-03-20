a796e200767c4d1494c6c78b8df1c55182f6a43c
dannyjy <danieljohn9295@gmail.com>
2026-03-20 12:15:45 +0200

    feat: add the forms method of getting data in react

---
+++ forms/src/App.tsx
@@ -0,0 +1,15 @@
+import SignUp from "./components/SignUp"
+
+const App = () => {
+  return (
+    <div>
+      <SignUp />
+      {/* <label htmlFor="fileInput" className="border-2 border-gray-400 border-dashed rounded-xl p-8 cursor-pointer bg-gray-100">
+        Choose you file
+        <input type="file" name="fileInput" id="fileInput" placeholder="Enter your file" className="hidden"/>
+      </label> */}
+    </div>
+  )
+}
+
+export default App
