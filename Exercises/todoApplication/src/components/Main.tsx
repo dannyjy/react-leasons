010f563369e4ab40816a3f7406405bcb1bdd43e0
dannyjy <danieljohn9295@gmail.com>
2026-03-23 17:46:48 +0200

    feat: add the todo application with the ability toadd tasks to the local storage

---
+++ Exercises/todoApplication/src/components/Main.tsx
@@ -0,0 +1,11 @@
+import AddTodo from '../ui/AddTodo'
+
+const Main = () => {
+  return (
+    <div className='flex items-center justify-center'>
+        <AddTodo />
+    </div>
+  )
+}
+
+export default Main
---
+++ Exercises/todoApplication/src/ui/AddTodo.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+
+const AddTodo = () => {
+    const [todo, setTodo] = useState<string[]>(JSON.parse(localStorage.getItem("tasks") as string) ?? []);
+
+    function getData(formData: FormData){
+        const { task } = Object.fromEntries(formData)
+        if(task === null || task === '') return;
+        setTodo((tasks) => [...tasks,task.toString()])
+    }
+
+    useEffect(() => {
+        localStorage.setItem("tasks", JSON.stringify(todo))
+    }, [todo])
+
+  return (
+    <div className="flex flex-col items-center"> 
+        <h1 className="text-5xl font-bold text-gray-400 py-16">Task Tracker</h1>
+        <form action={getData} className="flex gap-2">
+            <input type="text" name="task" placeholder="Enter your task" className="flex-2 bg-gray-100 border-2 rounded-xl border-[#d1d5db] py-1.5 px-2.5 min-w-96" />
+            <button className="bg-black px-6 py-2 text-white rounded-xl cursor-pointer">+ Add</button>
+        </form>
+        <ul>{todo.map((t,i) => <li key={i}>{t}</li>).reverse()}</ul>
+    </div>
+  )
+}
+
+export default AddTodo
