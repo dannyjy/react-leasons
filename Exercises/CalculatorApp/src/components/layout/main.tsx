7d495dde89dfdb0e46d1cc0d5ea94660ce1e85c4
dannyjy <danieljohn9295@gmail.com>
2026-04-03 19:38:22 +0200

    feat: add the main calculator logic and ui implementation

---
+++ Exercises/CalculatorApp/src/components/layout/main.tsx
@@ -0,0 +1,74 @@
+import { useReducer } from "react"
+import DigitButton from "../ui/CalculatorButton"
+import OperationButton from "../ui/OperationButton"
+import type { Action, State } from "../../types/types"
+import deleteIcon from "../../assets/delete.svg"
+
+function reducer(state: State, action: Action) {
+    const { type, payload } = action;
+    switch (type) {
+        case "Evaluate": 
+            return { ...state, answer: eval(`${state.currentOperand}`) }
+
+        case "Add Digit": {
+            if(payload)
+            if (state.currentOperand === "0" && payload!.digit === "0") return state
+            if (state.currentOperand.includes(".") && payload!.digit === ".") return state
+            return {
+                ...state,
+                currentOperand: `${state.currentOperand || ""}${payload!.digit}`
+            }
+        }
+
+        case "PositiveToggle":
+            return {...state, currentOperand: (parseFloat(state.currentOperand) * -1).toString()}
+
+        case "Percentage":
+            return { ...state, currentOperand: (parseFloat(state.currentOperand) / 100).toString()}
+
+        case "Delete Digit":
+            return {...state,currentOperand: state.currentOperand.slice(0,-1)}
+
+        case "Clear": 
+            return { ...state, currentOperand: "", answer: null }
+
+        default:
+            return state
+    }
+}
+
+const Main = () => {
+    const [state, dispatch] = useReducer(reducer, { currentOperand: "", answer: null });
+
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <main className="rounded-2xl p-2 space-y-3 text-gray-300 font-bold text-2xl">
+                <div className="w-96 h-40 space-y-5 bg-[#121414] rounded-2xl overflow-hidden p-4 flex flex-col justify-center items-end">
+                    <h2 className="text-3xl">{state.currentOperand}</h2>
+                    <h3 className="text-xl text-gray-500">{state.answer}</h3>
+                </div>
+                <div className="border-b-2 border-gray-800 flex justify-end pr-3 pb-2">
+                    <button className="" onClick={() => dispatch({ type: "Delete Digit" })}>
+                        <img src={deleteIcon} alt="delete" className="w-7" />
+                    </button>
+                </div>
+                <section className="grid grid-cols-4 gap-3">
+                    <OperationButton Text="AC" BgColor="bg-[#132335]" Dispatch={() => dispatch({ type: "Clear" }) } />
+                    <OperationButton Text="+/-" BgColor="bg-[#132335]" Dispatch={() => dispatch({ type: "PositiveToggle" }) } />
+                    <OperationButton Text="%" BgColor="bg-[#132335]" Dispatch={() => dispatch({ type: "Percentage" }) } />
+                    <OperationButton Text="/" BgColor="bg-[#132335]" Dispatch={() => dispatch({ type: "Add Digit", payload: { digit: "/" } }) } />
+                    {["7", "8", "9"].map((num, i) => <DigitButton key={i} Text={num} BgColor="bg-[#171919]" Dispatch={() => dispatch({ type: "Add Digit", payload: { digit: num } })} />)}
+                    <OperationButton BgColor="bg-[#132335]" Text="X" Dispatch={() => dispatch({ type: "Add Digit", payload: { digit: "*" } })} />
+                    {["4", "5", "6"].map((num, i) => <DigitButton key={i} Text={num} BgColor="bg-[#171919]" Dispatch={() => dispatch({ type: "Add Digit", payload: { digit: num } })} />)}
+                    <OperationButton BgColor="bg-[#132335]" Text="-" Dispatch={() => dispatch({ type: "Add Digit", payload: { digit: "-" } })} />
+                    {["1", "2", "3"].map((num, i) => <DigitButton key={i} Text={num} BgColor="bg-[#171919]" Dispatch={() => dispatch({ type: "Add Digit", payload: { digit: num } })} />)}
+                    <OperationButton BgColor="bg-[#132335]" Text="+" Dispatch={() => dispatch({ type: "Add Digit", payload: { digit: "+" } })} />
+                    {["0", "."].map((num, i) => <DigitButton key={i} Text={num} BgColor="bg-[#171919]" Dispatch={() => dispatch({ type: "Add Digit", payload: { digit: num } })} />)}
+                    <OperationButton Text="=" BgColor="bg-green-700 col-span-2 rounded-xl" Dispatch={() => dispatch({ type: "Evaluate" })} />
+                </section>
+            </main>
+        </div>
+    )
+}
+
+export default Main
