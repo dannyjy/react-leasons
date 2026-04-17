ab16855751b9883dbd32a0237128cc62ed09aba5
dannyjy <danieljohn9295@gmail.com>
2026-04-17 16:37:43 +0200

    feat: add the rest of the components for the mental model questions

--- mentalmodel/first/src/App.tsx
+++ mentalmodel/first/src/App.tsx
@@ -4,6 +4,13 @@ import Cart from './components/Cart'
 import Counter from './components/Counter'
 import InfiniteLoop from './components/InfiniteLoop'
 import { Logger } from './components/Logger'
+import Search from './components/Search'
+import WindowSize from './components/WindowSize'
+import Batching from './components/Batching'
+import Dashboard from './components/Dashboard'
+import Post from './components/Post'
+import FilteredList from './components/FilteredList'
+import { lists } from './data/data'
 
 const App = () => {
   const [items] = useState([{price: 500},{price: 500}]);
@@ -14,6 +21,12 @@ const App = () => {
       <Logger />
       <InfiniteLoop />
       <Cart items={items}/>
+      <Search query='food'/>
+      <WindowSize />
+      <Batching />
+      <Dashboard postId='1' />
+      <Post postId='6' />
+      <FilteredList items={lists} query='mango'/>
     </div>
   )
 }
